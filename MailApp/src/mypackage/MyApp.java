786be403493a0eaee771fd4f6c622bd1192d2958
RakeshRapelliwar <rakeshr@techepoch.com>
2013-02-28 15:52:49 +0530

    my app changed

--- MailApp/src/mypackage/MyApp.java
+++ MailApp/src/mypackage/MyApp.java
@@ -5,7 +5,7 @@ import net.rim.device.api.ui.UiApplication;
 
 public class MyApp extends UiApplication
 {
-   
+   //Hi this is meeeeeeeeeeee
     public static void main(String[] args)
     {
         
